41c43cb2e509f630afbd64cfccee23d6f7f42348
Alijah Green <atra2396@gmail.com>
2018-02-10 15:35:35 -0500

    We'll just do this in one file...

--- EchoCare/echoCare.py
+++ EchoCare/echoCare.py
@@ -1,9 +1,29 @@
-from flask import render_template
-from flask-ask import statement, question
-from EchoCare import app, ask, db
+from flask import Flask, render_template
+from flask-ask import Ask, statement, question
+from EchoCare import app, ask, db, db_info
+from peewee import *
+import pymysql
+
+DATABASE = 'sample.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+HOSTNAME = 'localhost'
+
+db_info = { 'database': DATABASE,
+			'username': USERNAME,
+			'password': PASSWORD,
+			'hostname': HOSTNAME }
+
+app = Flask(__name__)	# Define app instance
+ask = Ask(app, "/")		# flask-ask instance
+db = MySQLDatabase(DATABASE) # If we decide to use the ORM, we can update this later
+
 
 @ask.launch
 def launch():
-	return statement('You\'re running the echoCare program.')
+	greeting = render_template('greeting')
+	repeat = render_template('greeting_reprompt')
+	return question(greeting).reprompt(repeat)
 
-
+@ask.intent('MedicationQuestionIntent'):
+	
