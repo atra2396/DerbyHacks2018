86dcece446d47283c62bfb0010f3092283c4198b
Alijah Green <atra2396@gmail.com>
2018-02-10 14:57:53 -0500

    Some python

--- EchoCare/echoCare.py
+++ EchoCare/echoCare.py
@@ -1,2 +1,9 @@
 from flask import render_template
 from flask-ask import statement, question
+from EchoCare import app, ask, db
+
+@ask.launch
+def launch():
+	return statement('You\'re running the echoCare program.')
+
+
