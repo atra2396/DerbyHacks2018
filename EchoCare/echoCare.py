72a9b3f225dd9aa07f66dd70acd5be699a267d83
Alijah Green <atra2396@gmail.com>
2018-02-10 17:16:45 -0500

    Adding some of the basics

--- EchoCare/echoCare.py
+++ EchoCare/echoCare.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask-ask import Ask, statement, question
 from EchoCare import app, ask, db, db_info
 from peewee import *
@@ -16,8 +16,13 @@ db_info = { 'database': DATABASE,
 
 app = Flask(__name__)	# Define app instance
 ask = Ask(app, "/")		# flask-ask instance
-db = MySQLDatabase(DATABASE) # If we decide to use the ORM, we can update this later
 
+session.attributes['user'] = "Bob"
+session.attributes['user_id'] = "0" 
+
+def connect_db():
+	return pymysql.connect(host=db_info['hostname'], user=db_info['username'],
+						   password=db_info['password'], db=db_info['database'])
 
 @ask.launch
 def launch():
@@ -25,5 +30,16 @@ def launch():
 	repeat = render_template('greeting_reprompt')
 	return question(greeting).reprompt(repeat)
 
-@ask.intent('MedicationQuestionIntent'):
-	
+@ask.intent('MedicationAlertIntent')
+def get_medical_alerts():
+	all_alerts = {}
+	connection = connect_db()
+	try:	# Get all med alerts for the day
+		with connection.cursor() as cursor:
+			query = "SELECT meds.m_name, DATE_FORMAT(alerts.time, '%Y-%m-%d') " \
+					"FROM meds INNER JOIN alerts ON meds.m_id=alerts " \
+					"WHERE alerts.time LIKE CONCAT(CURDATE(), '%') AND alerts.p_id=%s"
+			cursor.execute(query, (session.attributes['user_id']))
+			all_alerts = cursor.fetchall()
+	finally:
+		connection.close()
