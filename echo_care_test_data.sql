4c6bf311fd3b4ba538c9348b1618b26561cc134d
Natini8 <insertclevertitle92@gmail.com>
2018-02-10 22:22:06 -0500

    Almost completed database schema and test cases

---
+++ echo_care_test_data.sql
@@ -0,0 +1,37 @@
+INSERT INTO nurses VALUES (	null,
+				"Elijah Staton",
+				"15023772071",
+				"insertclevertitle92@gmail.com",
+				"Cosmo",
+				"08:00",
+				"16:00",
+				"Shepherdsville, Kentucky");
+
+INSERT INTO meds VALUES (	null,
+				"Solaris",
+				"Take with a glass of water.",
+				"2 pills",
+				"Moderate");
+
+INSERT INTO questions VALUES (	null,
+				"How many times have you used the bathroom today?");
+
+INSERT INTO conditions VALUES (	null,
+				"Crohn's Disease",
+				1);
+
+INSERT INTO patients VALUES (	null,
+				"Ryan Shah",
+				"15024980361",
+				"ryan.shah@uky.edu",
+				"pants",
+				"Lexington, Kentucky",
+				1,
+				1);
+
+INSERT INTO alerts VALUES (	null,
+				'2018-02-11',
+				"1 Day",
+				1,
+				null,
+				1);
