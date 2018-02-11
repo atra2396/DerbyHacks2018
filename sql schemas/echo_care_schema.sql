0182612605a3667d6f16fd001da85ba2947add6c
Natini8 <insertclevertitle92@gmail.com>
2018-02-11 03:56:30 -0500

    Removed password from patients

--- sql schemas/echo_care_schema.sql
+++ sql schemas/echo_care_schema.sql
@@ -7,18 +7,17 @@ CREATE TABLE nurses (		n_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 
 CREATE TABLE meds (		m_id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 				m_name varchar(30) NOT NULL, 
-				m_directions text NOT NULL, 
+				m_directions varchar(280) NOT NULL, 
 				m_dosage varchar(20) NOT NULL, 
 				m_priority varchar(20) NOT NULL);
 
 CREATE TABLE questions (	q_id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-				q_text text NOT NULL);
+				q_text varchar(280) NOT NULL);
 
 CREATE TABLE patients (         p_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
                                 p_name varchar(20) NOT NULL,
                                 p_phone varchar(20) NOT NULL,
                                 p_email varchar(50) NOT NULL,
-                                p_password varchar(32) NOT NULL,
                                 p_location varchar(50) NOT NULL,
                                 n_id int NOT NULL,                             
                                 FOREIGN KEY (n_id) REFERENCES nurses(n_id));
